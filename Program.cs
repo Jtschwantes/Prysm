be5c5d6830a9f61f3514a50502f5a3af8bb9f6d6
Jake <jtschwantes@gmail.com>
2019-09-30 22:29:58 -0600

    spectrum first changes

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using static Spectrum.Spec;
+using static Spectrum.Colors;
+
+namespace Spectrum
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            Spec.Initialize();
+            
+            var color1 = RGB(100, 255, 200);
+            //var color2 = HEX("#555555");
+            Spec.WriteLine("It's lit", Blue);
+            Style(RGB(255), HEX("#111111"));
+            Console.WriteLine("It's not lit");
+            Spec.GradientLine("It's Gradient!", HEX("ffffff"), HEX("000000"));
+            Spec.WriteLine("Test", RGB(255, 0 , 0));
+            Spec.WriteLine("Test", RGB(255, 0 , 55));
+            Spec.WriteLine("Test", RGB(255, 0 , 100));
+            Spec.WriteLine("Test", RGB(255, 0 , 155));
+            Spec.WriteLine("Test", RGB(255, 0 , 200));
+            Spec.WriteLine("Test", RGB(255, 0 , 255));
+            Spec.WriteLine("Test", RGB(200, 0 , 255));
+            Spec.WriteLine("Test", RGB(155, 0 , 255));
+            Spec.WriteLine("Test", RGB(100, 0 , 255));
+            Spec.WriteLine("Test", RGB(55, 0 , 255));
+            Spec.WriteLine("Test", RGB(0, 0 , 255));
+
+            Spec.WriteLine("Test", HEX("00ff00"));
+            Spec.WriteLine("Test", HEX("00ee00"));
+            Spec.WriteLine("Test", HEX("00dd00"));
+            Spec.WriteLine("Test", HEX("00cc00"));
+            Spec.WriteLine("Test", HEX("00aa00"));
+            Spec.WriteLine("Test", HEX("009900"));
+            Spec.WriteLine("Test", HEX("007700"));
+            Spec.WriteLine("Test", HEX("006600"));
+            Spec.WriteLine("Test", HEX("005500"));
+            Spec.WriteLine("Test", HEX("003300"));
+            Spec.WriteLine("Test", HEX("001100"));
+
+            Spec.Reset();
+        }
+    }
+}
---
+++ Spectrum.cs
@@ -0,0 +1,124 @@
+using System;
+using System.Runtime.InteropServices;
+using System.Collections.ObjectModel;
+
+namespace Spectrum
+{
+    public static class Spec
+    {
+        [DllImport( "kernel32.dll", SetLastError = true )]
+        public static extern bool SetConsoleMode( IntPtr hConsoleHandle, int mode );
+        [DllImport( "kernel32.dll", SetLastError = true )]
+        public static extern bool GetConsoleMode( IntPtr handle, out int mode );
+        [DllImport( "kernel32.dll", SetLastError = true )]
+        public static extern IntPtr GetStdHandle( int handle );
+
+        // Initialize to implement DLL files for functionality
+        public static void Initialize()
+        {
+            var handle = GetStdHandle( -11 );
+            int mode;
+            GetConsoleMode( handle, out mode );
+            SetConsoleMode( handle, mode | 0x4 );
+        }
+
+        // TODO: Grab user's 
+        public static void Reset()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Black;
+        }
+
+        // Writers
+        public static void WriteLine(string str, string fore = "", string back = "")
+        {
+            Console.WriteLine(fore.Remove(0, 1));
+            Console.WriteLine(fore.Replace(" ", "38") + back.Replace(" ", "48") + str);
+            Spec.Reset();
+        }
+
+        public static void GradientLine(string str, string frontBeginning, string frontEnd, string backBeginning = "", string backEnd = "")
+        {
+            var RGB1 = FormatToRGB(frontBeginning);
+            var RGB2 = FormatToRGB(frontEnd);
+            
+            var dbl = new double[3] {(double)RGB1[0], (double)RGB1[1], (double)RGB1[2]};
+
+            var dr = (double)(RGB2[0] - RGB1[0]);
+            var dg = (double)(RGB2[1] - RGB1[1]);
+            var db = (double)(RGB2[2] - RGB1[2]);
+
+            dr = dr / str.Length;
+            dg = dg / str.Length;
+            db = db / str.Length;
+
+            //Console.WriteLine(dr.ToString() + " " + dg.ToString() + " " + db.ToString());
+            
+            
+            for(int i = 0; i < str.Length; i++)
+            {
+                string s = FormatToForeground(RGB(Convert.ToInt32(dbl[0]), Convert.ToInt32(dbl[1]), Convert.ToInt32(dbl[2])));
+                Console.Write(s + str[i]);
+                dbl[0] += dr;
+                dbl[1] += dr;
+                dbl[2] += dr;
+                //Console.WriteLine(dbl[0] + " " + dbl[1] + " " + dbl[2]);
+            }
+        }
+        
+        public static void Write(string str, string fore = "", string back = "")
+        {
+            Console.Write(fore.Replace(" ", "38") + back.Replace(" ", "38") + str);
+            Spec.Reset();
+        }
+
+        // Color Getters (Public)
+        public static string RGB(int r = 0, int g = 0, int b = 0)
+        {
+            return "\x1b[ ;2;" + r + ";" + g + ";" + b + "m";
+        }
+
+        public static string HEX(string hex)
+        {
+            if(hex[0] == '#') hex = hex.Remove(0, 1);
+            string r = int.Parse(hex.Substring(0, 2), System.Globalization.NumberStyles.HexNumber).ToString();
+            string g = int.Parse(hex.Substring(2, 2), System.Globalization.NumberStyles.HexNumber).ToString();
+            string b = int.Parse(hex.Substring(4, 2), System.Globalization.NumberStyles.HexNumber).ToString();
+            return "\x1b[ ;2;" + r + ";" + g + ";" + b + "m";
+        }
+
+        public static void Style(string fore = "", string back = "")
+        {
+            Console.Write(FormatToForeground(fore) + FormatToBackground(back));
+        }
+
+        // Formatters (Private)
+        private static string FormatToForeground(string str) { return str.Replace(" ", "38");}
+        private static string FormatToBackground(string str) { return str.Replace(" ", "48");}
+        private static int[] FormatToRGB(string str)
+        {
+            var strs = str.Split(";");
+            return new int[] {int.Parse(strs[2]), int.Parse(strs[3]), int.Parse(strs[4].Remove(strs[4].Length - 1))};
+        }
+    }
+
+    // Default Colors
+    public static class Colors
+    {
+        public const string Black = "\x1b[ ;2;0;0;0m";
+        public const string White = "\x1b[ ;2;255;255;255m";
+        public const string Red = "\x1b[ ;2;255;0;0m";
+        public const string Green = "\x1b[ ;2;0;255;0m";
+        public const string Blue = "\x1b[ ;2;0;0;255m";
+        public const string Yellow = "\x1b[ ;2;255;255;0m";
+        public const string Magenta = "\x1b[ ;2;255;0;255m";
+        public const string Cyan = "\x1b[ ;2;0;255;255m";
+        public const string Grey = "\x1b[ ;2;100;100;100m";
+        public const string DarkRed = "\x1b[ ;2;100;0;0m";
+        public const string DarkGreen = "\x1b[ ;2;0;100;0m";
+        public const string DarkBlue = "\x1b[ ;2;0;0;100m";
+        public const string DarkYellow = "\x1b[ ;2;100;100;0m";
+        public const string DarkMagenta = "\x1b[ ;2;100;0;100m";
+        public const string DarkCyan = "\x1b[ ;2;0;100;100m";
+    }
+}
