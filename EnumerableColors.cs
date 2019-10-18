04a13a3feb61a25d3cbc73d44caf110319e1b5c2
Seth Bolander <44790090+sbolande@users.noreply.github.com>
2019-10-18 14:58:26 -0600

    Update EnumerableColors.cs

--- EnumerableColors.cs
+++ EnumerableColors.cs
@@ -6,6 +6,8 @@ namespace Spectrum
     public class ColorSets
     {
         public static readonly string[] Rainbow = {Red, DarkOrange, Yellow, Green, Cyan, Magenta};
-        public static readonly string[] Rainbow = {Red, White, CadetBlue};
+        public static readonly string[] Flag = {Red, White, CadetBlue};
+        public static readonly string[] Forest = {ImperialBlue, EmeraldGreen, PastelGreen, AlgaeGreen, Linen};
+        public static readonly string[] Loud = {OutrageousOrange, TurboYellow, MeadowGreen};
     }
 }
